2d57ee9ce064ccc9c4e3774b61c11d97093f6550
siddarthreddygsr <siddarthreddygsr@gmail.com>
2023-11-15 20:55:33 +0530

    Initial Commit

---
+++ Verde-Lucid/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Verde-Lucid
+//
+//  Created by Siddarth Reddy on 15/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Verde-Lucid/Verde_LucidApp.swift
@@ -0,0 +1,17 @@
+//
+//  Verde_LucidApp.swift
+//  Verde-Lucid
+//
+//  Created by Siddarth Reddy on 15/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Verde_LucidApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
